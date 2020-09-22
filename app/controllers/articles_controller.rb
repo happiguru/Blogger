2affc87787059884fb320b58b271ec7ae0c18915
hguruman <hguruman@gmail.com>
2020-09-23 00:53:28 +0400

    Update article_controller with create method

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,6 +8,19 @@ class ArticlesController < ApplicationController
     end
 
     def new
+        @article = Article.new
+    end
 
+    def create
+        @article = Article.new
+        @article.title = params[:article][:title]
+        @article.body = params[:article][:body]
+        # Alternative way
+            # @article = Article.new(
+            #     title: params[:article][:title],
+            #     body: params[:article][:body]
+            # )
+        @article.save
+        redirect_to article_path(@article)
     end
 end
