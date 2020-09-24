0200acc760f6e711e4eff41af2030121c0158175
hguruman <hguruman@gmail.com>
2020-09-24 14:45:14 +0400

    Add stickler and stylelintrc

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,18 +1,17 @@
 class TagsController < ApplicationController
-    
-    def index
-        @tags = Tag.all
-    end
-    
-    def show
-        @tag = Tag.find(params[:id])
-    end
+  def index
+    @tags = Tag.all
+  end
 
-    def destroy
-       @tag = Tag.find(params[:id])
-        @tag.destroy
+  def show
+    @tag = Tag.find(params[:id])
+  end
 
-        flash.notice = "Tag '#{@tag.id}' deleted"
-        redirect_to tags_path
-    end
+  def destroy
+    @tag = Tag.find(params[:id])
+    @tag.destroy
+
+    flash.notice = "Tag '#{@tag.id}' deleted"
+    redirect_to tags_path
+  end
 end
