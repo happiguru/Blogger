e2e8a99c75d576a619bd291a5bbcc16143b4260d
hguruman <hguruman@gmail.com>
2020-09-24 14:20:50 +0400

    Add destroy method for the tag

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,8 +1,4 @@
 class Tag < ApplicationRecord
-    has_many :taggings
-    has_many :articles, through: :taggings
-
-    # def to_s
-    #     name
-    # end
+    has_many :taggings, dependent: :destroy
+    has_many :articles, through: :taggings, dependent: :destroy
 end
